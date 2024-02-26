a0ee0e4f2f2324cec19e81da392263da9c6e3918
Ahmad Hassan <144830322+ahmad9059@users.noreply.github.com>
2024-02-26 12:48:37 +0500

    binary-array-sorting question added

---
+++ geeksforgeeks/array/binary-array-sorting.cpp
@@ -0,0 +1,24 @@
+class Solution{
+  public:
+    
+    // A[]: input array
+    // N: input array
+    //Function to sort the binary array.
+    void binSort(int arr[], int N)
+    {
+       //Your code here
+       int num=0;
+        for(int i=0;i<N;i++)
+        {
+            num+=arr[i];
+        }
+        int zero;
+        zero=N-num;
+        for(int i=0;i<zero;i++)
+        arr[i]=0;
+        for(int i=zero;i<N;i++)
+        arr[i]=1;
+        
+    }
+
+};
