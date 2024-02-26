ec152f6b5899085eb267ea39501341b56483a6d4
Ahmad Hassan <144830322+ahmad9059@users.noreply.github.com>
2024-02-26 12:49:29 +0500

    move-all-negative-elements-to-end question added

---
+++ geeksforgeeks/array/move-all-negative-elements-to-end.cpp
@@ -0,0 +1,32 @@
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+        int j=0;
+        vector<int>ans;
+        //for positive values
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>=0)
+            {
+                ans.push_back(arr[i]);
+                j++;
+            }
+        }
+        //for negative values
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<0)
+            {
+                ans.push_back(arr[i]);
+                j++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=ans[i];
+        }
+        
+    }
+};
