00b86aa83030d11d14e673eda63b85d749abe9ca
Ahmad Hassan <144830322+ahmad9059@users.noreply.github.com>
2024-02-26 12:46:29 +0500

    Create yclically-rotate-an-array-by-one question added

---
+++ geeksforgeeks/array/yclically-rotate-an-array-by-one.cpp
@@ -0,0 +1,14 @@
+
+
+//User function Template for C++
+
+void rotate(int arr[], int n)
+{
+    
+    int num=arr[n-1];
+    for(int i=n-1;i>=0;i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[0]=num;
+}
