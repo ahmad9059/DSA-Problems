9e0a12384e9dedc13a0a628f5d40bfb148461735
Ahmad Hassan <144830322+ahmad9059@users.noreply.github.com>
2024-02-26 12:45:07 +0500

    Create missing-element-of-ap question added

---
+++ geeksforgeeks/array/missing-element-of-ap.cpp
@@ -0,0 +1,17 @@
+//User function template for C++
+
+class Solution{   
+public:
+    int findMissing(int arr[], int n) {
+        // code here
+        int diff= (arr[n-1]-arr[0])/n;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i+1]-arr[i]!=diff)
+            {
+                return arr[i]+diff;
+            }
+        }
+        
+}
+};
